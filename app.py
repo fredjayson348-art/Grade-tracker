f9c6c12d109e201d5988a8faa488f53c93ea4495
fredjayson348-art <fredjayson348@gmail.com>
2026-03-25 19:29:54 +0000

    remove duplicate init_db calls

--- app.py
+++ app.py
@@ -8,13 +8,10 @@ import os
 
 app = Flask(__name__)
 app.secret_key = 'fred_secret_key_2025'
-init_db()
 import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DB = os.path.join(BASE_DIR, 'grades.db')
-init_db()
 
-init_db()
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
